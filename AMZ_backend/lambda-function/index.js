f49141ba48e3e4c9d4ccaa4c38d554a97d2c33e6
MarcieD1 <marciatarisai@icloud.com>
2024-08-17 12:02:52 +0200

    Updating lambda function to return desired output

--- AMZ_backend/lambda-function/index.js
+++
@@ -1,9 +0,0 @@
-// index.js
-exports.handler = async (event) => {
-    // Your business logic here
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
