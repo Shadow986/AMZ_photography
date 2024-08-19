30064012b8f149f475e42391d36b9ba7295f01d8
MarcieD1 <marciatarisai@icloud.com>
2024-08-20 01:44:12 +0200

    Config of submit form

--- js/custom.js
+++ js/custom.js
@@ -138,7 +138,7 @@ const AUTH_TOKEN = 'rhVHdZG7kb32cRrOSq5Ch9IRMYm7WyfL1fSxL6gb';
 
 const submitData = async (formData) => {
     try {
-        const response = await fetch('http://41.175.92.215/submitForm', {
+        const response = await fetch('https://amzphotography.mooo.com/submitForm', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
