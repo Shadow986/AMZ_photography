0b6ed8c51f6aacb8534da7a25cc61b2a62e31060
MarcieD1 <marciatarisai@icloud.com>
2024-08-20 02:38:46 +0200

    Update

--- js/custom.js
+++ js/custom.js
@@ -138,7 +138,7 @@ const AUTH_TOKEN = 'rhVHdZG7kb32cRrOSq5Ch9IRMYm7WyfL1fSxL6gb';
 
 const submitData = async (formData) => {
     try {
-        const response = await fetch('https://u1wzkcuyud.execute-api.us-east-1.amazonaws.com/Prod', {
+        const response = await fetch('https://u1wzkcuyud.execute-api.us-east-1.amazonaws.com/Prod/submitForm', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
